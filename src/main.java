5090e7c0956bcadc1d1f638713509c1f03f2bd1f
Felix <felix_ohlsen@yahoo.de>
2020-06-03 13:08:58 +0200

    Apache CLI
    http://commons.apache.org/proper/commons-cli/

--- src/main.java
+++ src/main.java
@@ -2,20 +2,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+import org.apache.commons.cli.*;
+
 public class main {
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    Scanner sc = new Scanner(System.in);
+    private static final String SAVE = "s";
+    private static final String CANCEL = "c";
 
-    User user = new User("Peter", 10);
+    public static void main(String args[]) throws ParseException {
+        //BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-    PersistenceManager pm =  PersistenceManager.getInstance();
+        Scanner sc = new Scanner(System.in);
 
-    public static void main(String [] args)
-    {
+        //User user = new User("Peter", 10);
+        //PersistenceManager pm = PersistenceManager.getInstance();#
 
+        Options options = new Options();
+        options.addOption("t", true, "display current time");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse( options, args);
+
+        if(cmd.hasOption("t")) {
+            System.out.println("test");
+        }
+        else {
+            // print the date
+        }
+        System.out.println("Enter username");
+
+        String userName = sc.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
     }
-
-
 
 }
