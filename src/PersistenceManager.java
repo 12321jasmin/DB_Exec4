0b56a9f8bad017b7c54f5557bada4285cf52022e
Jasmin <jasmin@delling.net>
2020-06-04 10:04:00 +0200

    Update PersistenceManager.java

--- src/PersistenceManager.java
+++ src/PersistenceManager.java
@@ -1,7 +1,9 @@
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class PersistenceManager {
     // static variable single_instance of type Singleton
@@ -30,7 +32,7 @@ public class PersistenceManager {
 
     public int beginTransaction(){
         ptaid ++;
-        //add to activetrans
+        activeTrans.put(ptaid, new HashSet<Integer>());
         return ptaid;
     }
 
@@ -40,29 +42,50 @@ public class PersistenceManager {
     }
 
     public boolean endTransaction(int taid){
-        Set<Integer> changedPages = activeTrans.get(taid);
+        Set<Integer> modifiedPages = activeTrans.get(taid);
 
-        //get all keys (pageid) of the buffer belonging to the transaction ID
+        Iterator itr = modifiedPages.iterator();
 
+        //make all modified pages persistent
+        while(itr.hasNext()){
+            int pageid = (int) itr.next();
+            String data = buffer.get(pageid);
+            writeToFile(taid, pageid, data);
 
+            //remove from buffer
+            buffer.remove(pageid);
+
+        }
+
+        //state transaction as completed
+        activeTrans.remove(taid);
         return false;
     }
 
 
     public boolean write (int taid, int pageid, String data){
+        int LSN = getLSN();
         Hashtable<Integer, String> tcontent;
 
         //check if buffer contains transaction information
-        if(buffer.contains(taid)){
-            //tcontent = buffer.get(taid);
+        if(buffer.contains(pageid)){
+            //page modified, no change possible
+            return false;
         }
         else {
             //if not: create new Hashtable belonging to specific transaction
-            tcontent= new Hashtable<Integer, String>();
-            //buffer.put(taid, tcontent);
+
+            //add userdata to buffer
+            buffer.put(pageid, data);
+
+            //add modified page to transactionid
+            Set<Integer> modifiedPages = activeTrans.get(pageid);
+            modifiedPages.add(LSN);
+
+            //do logentry
+            writeLogTran(LSN, taid, pageid, data);
+            return true;
         }
-        //tcontent.put(pageid, data);
-        return false;
     }
 
 
@@ -72,12 +95,49 @@ public class PersistenceManager {
     }
 
 
-    public boolean writeLogTran(int LSN, int taid, int pageid, String data ){
-        return false;
+    public boolean writeLogTran(int LSN, int taid, int pageid, String data ) throws IOException {
+
+        File logFile = new File("logFile.txt");
+        if (!logFile.exists()) {
+            logFile.createNewFile();
+        }
+
+        FileWriter logWriter = new FileWriter(logFile);
+        String logEntry = Integer.toString(LSN) + ", " + Integer.toString(taid) +
+                ", " +  Integer.toString(pageid) + ", " + data;
+        logWriter.write(logEntry);
+
+
+        //new line
+        logWriter.write(System.getProperty("line.separator"));
+
+        //close Writer
+        logWriter.close();
+
+        return true;
     }
 
-    public boolean writeLogEOT(int LSN, int taid, int pageid){
-        return false;
+    public boolean writeLogEOT(int LSN, int taid) throws IOException {
+
+        File logFile = new File("logFile.txt");
+        if (!logFile.exists()) {
+            logFile.createNewFile();
+        }
+
+        FileWriter logWriter = new FileWriter(logFile);
+        String logEntry = Integer.toString(LSN) + ", " + Integer.toString(taid) +
+                ", EOT" ;
+        logWriter.write(logEntry);
+
+
+        //new line
+        logWriter.write(System.getProperty("line.separator"));
+
+        //close Writer
+        logWriter.close();
+
+        return true;
     }
+
 
 }
